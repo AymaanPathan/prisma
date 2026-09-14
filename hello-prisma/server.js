594489c07c973fb10b7f7fa3a8bd23d5cb701aa3
aymaan <aymaanpathan5@gmail.com>
2026-09-14 11:31:34 +0530

    feat: add dotenv and prisma dependencies, configure server for environment variables

--- hello-prisma/server.js
+++ hello-prisma/server.js
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Hi server is running");
