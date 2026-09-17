aa43fc3d75f40ef616c435be4466f93ac82fee7e
aymaan <aymaanpathan5@gmail.com>
2026-09-17 13:45:54 +0530

    feat: add login functionality with validation and response handling

--- money-tracker/backend/controller/user.controller.js
+++ money-tracker/backend/controller/user.controller.js
@@ -66,3 +66,57 @@ export const registerUserController = async (req, res) => {
     });
   }
 };
+
+// Login user
+export const loginUserController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || email.trim() === "") {
+      return res.status(400).json({
+        status: 400,
+        message: "Email is required to login user",
+      });
+    }
+
+    if (!password || password.trim() === "") {
+      return res.status(400).json({
+        status: 400,
+        message: "password is required to login user",
+      });
+    }
+
+    const findUser = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!findUser) {
+      return res.status(404).json({
+        status: 404,
+        message: "No user found with this email",
+      });
+    }
+
+    const isPassValid = await bcrypt.compare(password, findUser.password);
+
+    if (!isPassValid) {
+      return res.status(401).json({
+        status: 401,
+        message: "Invalid password",
+      });
+    }
+
+    return res.status(200).json({
+      status: 200,
+      message: "Login sucessfull",
+    });
+  } catch (error) {
+    console.log("Internal server error while Login user", error);
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error while Login user",
+    });
+  }
+};
--- money-tracker/backend/routes/user.routes.js
+++ money-tracker/backend/routes/user.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { registerUserController } from "../controller/user.controller.js";
+import {
+  loginUserController,
+  registerUserController,
+} from "../controller/user.controller.js";
 
 const route = Router();
 route.post("/register", registerUserController);
+route.post("/login", loginUserController);
 
 export default route;
