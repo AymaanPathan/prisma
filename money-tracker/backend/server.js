52202c0d6cee8a4f3b7a25da3530d2a30d41fcc7
aymaan <aymaanpathan5@gmail.com>
2026-09-17 12:02:08 +0530

    feat: update package.json to use ES modules and add express dependency
    - Changed module type to "module" in package.json
    - Added "server" script to start the server with nodemon
    - Added express as a dependency
    
    feat: create server.js to set up a basic Express server
    
    - Implemented a simple Express server that responds with a message on the root route
    - Configured the server to listen on port 3000

---
+++ money-tracker/backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from money tracker root route");
+});
+
+app.listen(port, () => {
+  console.log(`money tracker server is started on port ${port} `);
+});
