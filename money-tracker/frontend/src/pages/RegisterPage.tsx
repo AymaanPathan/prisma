ab60b17b08349e6a0c82cece5fcf2d42f3505afa
aymaan <aymaanpathan5@gmail.com>
2026-09-19 13:26:35 +0530

    feat: create RegisterPage component with form handling for user registration

--- money-tracker/frontend/src/pages/RegisterPage.tsx
+++ money-tracker/frontend/src/pages/RegisterPage.tsx
@@ -1,7 +1,30 @@
 export const RegisterPage = () => {
+  const handleRegister = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("registered");
+  };
+
   return (
     <div className="flex h-screen items-center justify-center ">
-      <h1>Register page</h1>
+      <div className="border p-12">
+        <form onSubmit={handleRegister} className="flex flex-col gap-6 ">
+          <div className="flex gap-2">
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" placeholder="Enter username" />
+          </div>
+          <div className="flex gap-2">
+            <label htmlFor="email">Email</label>
+            <input id="email" type="text" placeholder="Enter email" />
+          </div>
+          <div className="flex gap-2">
+            <label htmlFor="password">password</label>
+            <input type="password" placeholder="Enter password" />
+          </div>
+          <button className="bg-blue-600 rounded-2xl p-3 text-white cursor-pointer active:scale-95">
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
